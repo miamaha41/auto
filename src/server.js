4fc8e12f6826649b42150968b14d0e9e4bf92a5c
Nguyen Thi Hai Yen <miamaha41@gmail.com>
2022-10-18 09:19:21 +0700

    change version.json

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ import cors from "cors";
 import { handleError, showError } from "./middlewares/handleError.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 // app.get("/", (req, res, next) => {
